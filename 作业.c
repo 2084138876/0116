0b4092627a1898e5949757f4ce3fc8abb1806107
2084138876 <2084138876@qq.com>
2025-01-16 16:15:26 +0800

    Add files via upload

---
+++ 作业.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	int day = 0;
+//	scanf("%4d%2d%2d", &year, &month, &day);
+//	printf("year=%d\n", year);
+//	printf("month=%02d\n", month);//月份是两位整数，当月份是一位数时使用0进行填充
+//	printf("day=%02d\n", day);
+//
+//
+//
+//	return 0;
+
+//int main()
+//{
+//	int id = 0;
+//	float c = 0.0f;
+//	float match = 0.0f;
+//	float english = 0.0f;
+//	scanf("%d %f %f %f", &id, &c, &match, &english);
+//	printf("%d %.3f %.3f %.3f", id, c, match, english);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = printf("Hello world!");//printf函数有返回值，返回值是打印字符的个数  int类型
+//	printf("\n%d", n);
+//
+//
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[4] ={0};
+//	int i = 0;
+//	while (i<4)
+//	{
+//		scanf("%d", &arr[i]);
+//		i++;
+//	}
+//	int j = 0;
+//	int max = arr[0];
+//	while (j< 4)
+//	{
+//		if (arr[j] > arr[0])
+//		{
+//			max = arr[j];
+//		}
+//		j++;
+//	}
+//
+//	printf("%d %d %d %d 中的max=%d", arr[0], arr[1], arr[2], arr[3], max);
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	float r = 0.0f;
+//	float V = 0.0f;
+//	scanf("%f", &r);
+//	V = 4 / 3.0 * 3.1415926 * r * r * r;
+//	printf("半径是：%.2f\n体积是：%.2f\n", r, V);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	float weight = 0.0f;//体重  单位是kg
+//	float high = 0.0f;//身高，单位是m
+//	scanf("%f %f", &weight, &high);
+//	float BMI = weight / high / high;
+//	printf("BMI=%.2f", BMI);
+//
+//
+//
+//	return 0;
+//}
